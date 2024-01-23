b71471e51d17c4933d87d3b7694ccc7ac9b86414
Ling Huang <linghuang@Lings-MacBook-Pro.local>
2024-01-22 19:29:22 -0800

    initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
+
+
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
+
+
+  return (
+    <div>
+      { 
+      puppies.map((puppy) => {
+        return <p>{puppy.name} {puppy.age}</p>
+      })
+      }
+    </div>
+  )
+}
+
+export default App
